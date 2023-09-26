78bed17deeb607a114ad45fc6d946427b7245419
reetikgit <reetikchitragupt@gmail.com>
2023-09-26 23:19:21 +0530

    ABC

---
+++ src/views/dashboards/Calender.vue
@@ -0,0 +1,27 @@
+<script setup>
+    import Breadcrumb from '@/components/Breadcrumbs.vue'
+    import { ref } from 'vue'
+    import { TabGroup, TabList, Tab, TabPanels, TabPanel } from '@headlessui/vue'
+    let categories = ref(['Timeline', 'About', 'Friends', 'Photos'])
+    
+</script>
+<template>
+<div class="container mx-auto">
+    <Breadcrumb parentTitle='Calender' subParentTitle='Date Picker' />
+    <div class="grid grid-cols-12 gap-5">
+    <div class="col-span-6">
+        <h2> Start date </h2>
+        <div id="app">
+  <v-app id="inspire">
+    <v-row justify="center">
+      <v-date-picker v-model="picker"></v-date-picker>
+    </v-row>
+  </v-app>
+</div>
+    </div>
+    <div class="col-span-6">
+        <h2> End date </h2>
+    </div>
+    </div>
+</div>
+</template>
